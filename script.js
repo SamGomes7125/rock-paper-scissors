1c5494c1c8b99b715faff3ee96db65a388b85cbf
SamGomes7125 <swarnabhaghosh2005@gmail.com>
2025-10-22 11:07:11 +1100

    Add getComputerChoice function with random selection logic

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 console.log("Hello World");
+function getComputerChoice() {
+  const randomNumber = Math.random();
+
+  if (randomNumber < 0.33) {
+    return "rock";
+  } else if (randomNumber < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+
+console.log(getComputerChoice());
 
